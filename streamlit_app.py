211347d677c820090fbd5d1c3081ef65c7b7c69b
Dr. Mohammed AL Jebori <35558952+ALJEBORI@users.noreply.github.com>
2026-05-25 18:46:32 +0200

    Refactor smoothie order submission process
    Refactor smoothie order submission logic and remove duplicate API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,15 @@ if ingredients_list:
     ingredients_string=''#str(ingredient_list)
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen+' ' 
+        url="https://my.smoothiefroot.com/api/fruit/"+fruit_choosen
+        smoothiefroot_response = requests.get(url)  
+    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write("String ", ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     #st.write(my_insert_stmt) 
     time_to_insert=st.button('Submit Order')
-    url="https://my.smoothiefroot.com/api/fruit/"+fruit_choosen
-    smoothiefroot_response = requests.get(url)  
-    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ 
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
